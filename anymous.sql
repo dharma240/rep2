5aa26acf926ef185fb7a2229558b0857909054ed
Dharma <you@example.com>
2025-06-26 18:50:21 +0000

    new_file added by dharma

---
+++ anymous.sql
@@ -0,0 +1,25 @@
+/*declare 
+a number;
+b number :=&which_table;---- 2 tables or 3 tables
+begin
+for a in 1..10 loop
+dbms_output.put_line(a||'*'||b||'='||a*b);
+end loop;
+end;
+/*/
+/*
+declare 
+a varchar2(15):='raja';
+begin
+for i in 1..length(a) loop
+dbms_output.put_line(substr(a,i,1));
+end loop;
+end;
+*/
+
+
+
+
+
+
+
